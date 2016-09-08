266cb69edbc39f0a2d261350eedcbd5c1c9d0b22
rusania <rusania@gmail.com>
2016-09-08 14:05:06 +0800

    mpa scripts

---
+++ mpa_profit_calc.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        mpa_profit_calc
+// @namespace   mpa_profit_calc
+// @description mpa profit calc
+// @include     https://*mypayingads.com/module3/position
+// @include     https://*mytrafficbux.com/module3/position
+// @icon        http://www.mypayingads.com/favicon.ico
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_addStyle
+// @grant GM_xmlhttpRequest
+// @grant unsafeWindow
+// @updateURL https://github.com/rusania/gm_scipts/raw/master/mpa_profit_calc.user.js
+// @downloadURL https://github.com/rusania/gm_scipts/raw/master/mpa_profit_calc.user.js
+// @version     2016.09.08
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+$($('.membertopmenu').children()[0]).append('<li><a id="calc">CALC</li>');
+$('#calc').click(function(){
+    var e1 = 0;
+    var e2 = 0;
+    $('.divtbody').children().each(function () {
+        var p = $(this).children();
+        var q = /([0-9.]+)/.exec($(p[3]).text()) [1];
+        var r = /([0-9.]+)/.exec($(p[4]).text()) [1];
+        var s = /([0-9.]+)/.exec($(p[5]).text()) [1];
+        var amount = parseFloat(q);
+        var earn = parseFloat(r)+parseFloat(s);
+        e1 += amount;
+        e2 += earn;
+        var percent = (earn / amount).toFixed(2);
+        $(p[8]).replaceWith('<div class="divtd textcenter vam"><b>'+percent+'</b></div>');
+    });
+    $('.floatleft.searchlabel').replaceWith('<div class="floatleft searchlabel">投入：' + e1 + ' 收益：' + e2.toFixed(0) + ' 完成：' + (e2 / e1).toFixed(2) + '</div>');
+});
---
+++ mpa_surf.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        mpa_surf
+// @namespace   mpa_surf
+// @description mpa surf tip
+// @include     http*://*mypayingads.com/traffic/surf
+// @icon        http://www.mypayingads.com/favicon.ico
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_addStyle
+// @grant GM_xmlhttpRequest
+// @grant unsafeWindow
+// @updateURL https://github.com/rusania/gm_scipts/raw/master/mpa_surf.user.js
+// @downloadURL https://github.com/rusania/gm_scipts/raw/master/mpa_surf.user.js
+// @version     2016.09.08
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+var counter = setInterval(function () {
+  var t = $('#credittopframe').text();
+  if (t.length == 0) {
+    clearInterval(counter);
+    $('.mainImage').each(function () {
+      var match = /chk_(\d+)/.exec($(this).attr('id'));
+      if (match) {
+        var id = match[1];
+        var counter2 = setInterval(function () {
+          t = $('#credittopframe').text();
+          if (t.length > 10) {
+            clearInterval(counter2);
+            match = /(\d+) ads/.exec($('.link3').html());
+            if (match) {
+              var limit = match[1];
+              if (limit > 15) {
+                alert('surf limit');
+              } 
+              else {
+                $('.link4') [0].click();
+              }
+            }
+          }
+        }, 3000);
+        checkImage(id);
+      }
+    });
+  }
+}, 2000);
