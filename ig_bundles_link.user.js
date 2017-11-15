daedfe6e37783c5002403d8a67d6c357c8108563
rusania <rusania@gmail.com>
2017-11-15 15:21:45 +0800

    20171115a

--- ig_bundles_link.user.js
+++ ig_bundles_link.user.js
@@ -7,16 +7,22 @@
 // @icon        http://www.indiegala.com/favicon.ico
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/ig_bundles_link.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/ig_bundles_link.user.js
-// @version     2017.11.15.1
+// @version     2017.11.15.2
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
 // @grant       none
 // ==/UserScript==
+
+var uid = '';
+var m = /user_id=([0-9a-f]+)/.exec(document.URL);
+if (m)
+    uid = m[1];
+
 var tp = $('#title-p');
 if (tp.length > 0){
     $('#title-p').after('<div><a id="ajax">AJAX</a></div>');
     // \"bundles-list\"
-    var m = /id=([0-9a-f]+)/.exec(document.URL);
+    m = /id=([0-9a-f]+)/.exec(document.URL);
     if (m)
         $('#title-p').after('<div><a href="/ajaxprofile_sale_tab?user_id=' + m[1] + '" target="_blank">URL</a></div>');
 }
@@ -27,10 +33,13 @@ var bl = $('#bundles-list');
 if (bl.length > 0){
     bl.before('<table id="bl">AA</table>');
     bl.before('<div><a id="ajax">AJAX</a></div>');
+    bl.before('<form id="f" action="http://173.82.212.166/ig_sale.php?c=sale&u=' + uid + '" method="post" target="_blank"></form>');
     $('.nav-toggle').each(function () {
         var id = $(this).attr('id');
         $("#bl").append('<tr><td>' + id + '</td><td><a href="/ajaxsale?sale_id=' + id + '" target="_blank">' + $(this).text() + '</a></td</tr>');
+        $('#f').append('<input type="hidden" name="' + id + '" value="' + $(this).text() + '" />');
     });
+    $('#f').append('<input type="submit" value="Submit" />');
 }
 
 $('#ajax').click(function(){
