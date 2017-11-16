5d3fcbd33cfd22b83ed6d086b35dd5bce7c1d324
rusania <rusania@gmail.com>
2017-11-17 07:42:48 +0800

    20171117a

--- ig_bundles_link.user.js
+++ ig_bundles_link.user.js
@@ -7,11 +7,16 @@
 // @icon        http://www.indiegala.com/favicon.ico
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/ig_bundles_link.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/ig_bundles_link.user.js
-// @version     2017.11.15.2
+// @version     2017.11.16.1
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
-// @grant       none
+// @grant       GM_xmlhttpRequest
+// @grant       GM_addStyle
 // ==/UserScript==
+
+GM_addStyle("table{border:solid 1px;border-collapse:collapse !important;}");
+GM_addStyle("td{border:solid 1px;border-collapse:collapse;padding-left:5px;padding-right:5px;font-family:simsun !important;}");
+GM_addStyle("div{font-family:simsun !important;}");
 
 var uid = '';
 var m = /user_id=([0-9a-f]+)/.exec(document.URL);
@@ -30,17 +35,37 @@ document.body.innerHTML = document.body.innerHTML.replace(/\\r|\\n|\\t/ig, '');
 document.body.innerHTML = document.body.innerHTML.replace(/\\&quot;/ig, '');
 $('#collapseGiveaway').empty();
 var bl = $('#bundles-list');
-if (bl.length > 0){
-    bl.before('<table id="bl">AA</table>');
-    bl.before('<div><a id="ajax">AJAX</a></div>');
-    bl.before('<form id="f" action="http://173.82.212.166/ig_sale.php?c=sale&u=' + uid + '" method="post" target="_blank"></form>');
-    $('.nav-toggle').each(function () {
-        var id = $(this).attr('id');
-        $("#bl").append('<tr><td>' + id + '</td><td><a href="/ajaxsale?sale_id=' + id + '" target="_blank">' + $(this).text() + '</a></td</tr>');
-        $('#f').append('<input type="hidden" name="' + id + '" value="' + $(this).text() + '" />');
-    });
-    $('#f').append('<input type="submit" value="Submit" />');
+var nv = $('#accordion2').find('.nav-toggle');
+if (nv.length > 0){
+    if (bl.length > 0){
+        bl.before('<table id="bl"></table>');
+        bl.before('<div><a id="ajax">AJAX</a></div>');
+        bl.before('<div><a id="lib">LIB</a></div>');
+        bl.before('<form id="f" action="http://173.82.212.166/ig_sale.php?c=sale&u=' + uid + '" method="post" target="_blank"></form>');
+        nv.each(function () {
+            var id = $(this).attr('id');
+            $("#bl").append('<tr id="' + id + '"><td>' + id + '</td><td><a href="/ajaxsale?sale_id=' + id + '" target="_blank">' + $(this).text() + '</a></td</tr>');
+            $('#f').append('<input type="hidden" name="' + id + '" value="' + $(this).text() + '" />');
+        });
+        $('#f').append('<input type="submit" value="Submit" />');
+    }
 }
+
+$('#lib').click(function(){
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'http://173.82.212.166/ig_sale.php?c=ss&p=pf&q=' + uid,
+        onload: function (response) {
+            var data = JSON.parse(response.responseText);
+            $.each(data, function (i, g) {
+                var tr = $('#' + g.id);
+                tr.append('<td>' + g.dt + '</td>');
+                tr.append('<td>' + g.gift + '</td>');
+                tr.append('<td>' + g.lmt + '</td>');
+            });
+        }
+    });
+});
 
 $('#ajax').click(function(){
     $('.nav-toggle').each(function () {
