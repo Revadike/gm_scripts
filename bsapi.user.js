85375814fd0156814d85863005d9baeb380eed50
rusania <rusania@gmail.com>
2017-11-30 16:25:36 +0800

    20171130

--- bsapi.user.js
+++ bsapi.user.js
@@ -2,37 +2,45 @@
 // @name        bsapi
 // @namespace   bsapi
 // @description bs api
-// @include     https://www.bundlestars.com/en/orders/*
+// @include     https://www.fanatical.com/en/orders/*
 // @icon        https://cdn.bundlestars.com/production/brand/favicon.ico
-// @grant GM_getValue
-// @grant GM_setValue
-// @grant GM_addStyle
-// @grant GM_xmlhttpRequest
-// @grant unsafeWindow
 // @updateURL https://github.com/rusania/gm_scipts/raw/master/bsapi.user.js
 // @downloadURL https://github.com/rusania/gm_scipts/raw/master/bsapi.user.js
-// @version     2017.02.28.1
-// @grant       none
+// @version     2017.11.30.1
+// @run-at      document-end
+// @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
 // ==/UserScript==
-var li = $('#navbarBundles').parent().parent();
-li.append('<li><a id ="fetch" href="#"><span style="color:green;font-weight:bold;">FETCH</span></a></li>');
-li.append('<li><a id ="redeem" href="#"><span style="color:yellow;font-weight:bold;">REDEEM</span></a></li>');
+$('#root').before('<ul id="m"></ul>');
+$('#m').append('<li><a id ="fetch" href="#"><span style="color:green;font-weight:bold;">FETCH</span></a></li>');
+$('#m').append('<li><a id ="redeem" href="#"><span style="color:yellow;font-weight:bold;">REDEEM</span></a></li>');
+// <div ng-if="order.status === 'COMPLETE' || order.status === 'complete'" ng-hide="game.key" class="key ng-scope ng-hide"><!-- ngIf: !legacy --><a ng-click="redeemSerial(order._id, item._id, game)" ng-if="!legacy" class="ng-scope">Reveal Key</a><!-- end ngIf: !legacy --><!-- ngIf: legacy --></div>
 
 $('#fetch').click(function () {
-    $('.key-container.hidden-sm.hidden-xs div a').click();
+    $('.btn.btn-secondary.btn-block').click();
 });
 
 $('#redeem').click(function () {
-    if ($('#list').length > 0) {
-        $('#list').remove();
-    }
-    $('.order').append('<table id="list"></table>');
-    $('.bundle').each(function () {
-        $(this).parent().find('.order-table').each(function (i, e) {
-            var title = $(e).find('.title').text();
+    if ($('#b').length > 0)
+        $('#b').empty();
+    else
+        $('.mt-4').after('<table id="b"></table>');
+
+    var h = $('h5');
+    if (h.length > 0){
+        $('h5').each(function(){
+            $(this).parent().find('.order-item').each(function (i, e) {
+                var title = $(e).find('.d-flex.flex-column').text();
+                var key = $(e).find('.form-control').val();
+                var f = '<tr><td>' + (i+1) +'</td><td>' + title + '</td><td>' + key + '</td><td>【' + title.replace(',', '') + '】' + key + '</td></tr>';
+                $('#b').append(f);
+            });
+        });
+    } else {
+        $('.order-item').each(function (i, e) {
+            var title = $(e).find('.d-flex.flex-column').text();
             var key = $(e).find('.form-control').val();
             var f = '<tr><td>' + (i+1) +'</td><td>' + title + '</td><td>' + key + '</td><td>【' + title.replace(',', '') + '】' + key + '</td></tr>';
-            $('#list').append(f);
+            $('#b').append(f);
         });
-    });
+    }
 });
