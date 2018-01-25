999071522f7c8a2168db74a291063a72662dfff7
rusania <rusania@gmail.com>
2018-01-25 13:50:56 +0800

    20180125

--- otk_gg_game_keys.user.js
+++ otk_gg_game_keys.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         otk_gg_game_keys
 // @namespace    http://tampermonkey.net/
-// @version      2018.01.10.1
+// @version      2018.01.25.2
 // @description  otk_gg_game_keys
-// @author       You
+// @author       jacky
 // @match        http*://*gogobundle.com/*/order/show/*
 // @include      http*://*gogobundle.com/*/order/show/*
 // @match        http*://*otakubundle.com/*/order/show/*
+// @include      http*://*otakubundle.com/*/order/show/*
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/otk_gg_game_keys.user.js
@@ -20,24 +21,31 @@ $('legend').each(function(){
         var a = [];
         var b = [];
 
+        $(this).after('<div id="c"></div>');
+        $(this).after('<div id="k"></div>');
         t.each(function(){
             var d = $(this).find('td');
             var n = $(d[0]).text();
             var s = $(d[1]).text();
-            if ($.inArray(n, a) < 0){
-                a.push(n);
-                b[n]= [];
+            var m = /Coupon/.exec(n);
+            if (m){
+                $('#k').append('<span style="color:red">' + n + '：' + s + '</span>');
+            } else {
+                if ($.inArray(n, a) < 0){
+                    a.push(n);
+                    b[n]= [];
+                }
+                b[n].push(s);
             }
-            b[n].push(s);
         });
-        $(this).after('<div id="k"></div>');
+
         var ks = b[a[0]].length;
         var js = a.length;
         for (i=0; i < ks; i++) {
             var c = [];
             for (j=0; j < js; j++) {
                 var k = b[a[j]][i];
-                var key = '<div>【' + (j+1) + '】【' + a[j]  +'】&nbsp' + k + '</div>';
+                var key = '<div>【' + (j+1) + '】【' + a[j]  +'】&nbsp;' + k + '</div>';
                 c.push(k);
                 $('#k').append(key);
             }
