057a5095d61c82c6b71c163a4168934e143f78d8
rusania <rusania@gmail.com>
2018-01-30 14:04:17 +0800

    20180130

--- stdb_more_info.user.js
+++ stdb_more_info.user.js
@@ -8,14 +8,16 @@
 // @match        http*://steamdb.info/search/*
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/stdb_more_info.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/stdb_more_info.user.js
-// @version     2018.01.23.1
+// @version     2018.01.30.1
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
 // ==/UserScript==
 
+var m = /(sub|app)\/(\d+)/.exec(document.URL);
 var p = $('.package');
 if (p.length > 0){
     $('.app-links').append('<a id="cmp">Cmp</a>');
+    $('.app-links').append('<a id="pkg" target="_target" href="http://167.88.168.94/package.php?id=' + m[2] + '">Pkg</a>');
     p.each(function(){
         var id = $(this).attr('data-subid');
         $(this).append('<td><input type="checkbox" value="' + id + '">sub/' + id + '</td>');
