30bb2bef248db32cf7ab5f1cdfec5e23fd405e55
rusania <rusania@gmail.com>
2018-03-16 16:01:48 +0800

    20180316

--- stdb_more_info.user.js
+++ stdb_more_info.user.js
@@ -5,14 +5,25 @@
 // @author       jacky
 // @match        http*://steamdb.info/app/*
 // @match        http*://steamdb.info/sub/*
+// @match        http*://steamdb.info/freepackages*
 // @match        http*://steamdb.info/search/*
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/stdb_more_info.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/stdb_more_info.user.js
-// @version     2018.01.30.1
+// @version     2018.03.16.01
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
 // ==/UserScript==
 
+if (/freepackages/.exec(document.URL)){
+    $('h1').after('<a id="rm">REMOVE</a>');
+    $('#rm').click(function(){
+        $('.package').each(function(){
+            if (/Trailer|Demo|Trial/ig.exec($(this).html())){
+                $(this).children('button').click();
+            }
+        });
+    });
+} else {
 var m = /(sub|app)\/(\d+)/.exec(document.URL);
 var p = $('.package');
 if (p.length > 0){
@@ -39,3 +50,4 @@ $('.app').each(function(){
     var id = $(this).attr('data-appid');
     $(this).append('<td>app/' + id + '</td>');
 });
+}
