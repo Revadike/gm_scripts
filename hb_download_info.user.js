921b00822635d3b558a6adcd31b63ff234854c23
rusania <rusania@gmail.com>
2018-03-28 14:46:29 +0800

    20180328

--- hb_download_info.user.js
+++ hb_download_info.user.js
@@ -7,14 +7,14 @@
 // @include     http*://www.humblebundle.com/*?key=*
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/hb_download_info.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/hb_download_info.user.js
-// @version     2018.02.08.1
+// @version     2018.03.28.1
 // @run-at      document-end
 // @require     http://cdn.bootcss.com/jquery/3.1.0/jquery.min.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
 // ==/UserScript==
 
-GM_addStyle("table{border:solid 1px;border-collapse:collapse !important;}");
+GM_addStyle("table{border:solid 1px;border-collapse:collapse;font-size:16px !important;}");
 GM_addStyle("td{border:solid 1px;border-collapse:collapse;padding-left:5px;padding-right:5px !important;}");
 GM_addStyle(".d{font-size:16px;color:white !important;}");
 
@@ -25,48 +25,52 @@ if (m) {
 
 m = /downloads/.exec(document.URL);
 if (m){
-    var r = /var data = ({"keys".*});/.exec(document.body.innerHTML);
-    if (r){
-        $('#headertext').append('<table id="reg"></table>');
-        $('#reg').append('<tr><td>App</td><td>machineName</td><td>exclusive</td><td>disallowed</td></tr>');
-        var data = JSON.parse(r[1]);
-        $.each(data.keys, function (i, item) {
-            var exc = '<td>-</td>';
-            if (item.exclusiveCountries.length){
-                exc = '<td title="' + item.exclusiveCountries + '">List</td>';
-            }
-            var dis = '<td>-</td>';
-            if (item.disallowedCountries.length){
-                dis = '<td title="' + item.disallowedCountries + '">List</td>';
-            }
-            $('#reg').append('<tr><td>' + item.steamAppId + '</td><td>' + item.machineName + '</td>' + exc + dis+'</tr>');
-        });
-    }
-    /*
-	r = /data.countryCode = "([^"]+)";/.exec(document.body.innerHTML);
-	if (r)
-		$('#headertext').append('<div><b>' + r[1] + '</b></div>');
-    */
-
+    $('#headertext').append('<table id="reg"></table>');
+    $('#headertext').append('<div><a id="r">LOCK</a></div>');
     $('#headertext').append('<div><a id="btn">INFO</a></div>');
     $('#headertext').append('<div><a id="key">KEYS</a></div>');
     $('#headertext').append('<div><a id="gift">GIFT</a></div>');
     $('#headertext').append('<table id="info"></table>');
 
+    $('#r').click(function () {
+        $('#reg').empty();
+        $('#reg').append('<tr><td>App</td><td>machineName</td><td>exclusive</td><td>disallowed</td></tr>');
+        var m = /key=([0-9A-Z]+)/i.exec(document.URL);
+        var url = 'https://www.humblebundle.com/api/v1/order/' + m[1] + '?all_tpkds=true';
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function(data){
+                $.each(data.tpkd_dict.all_tpks, function (i, item) {
+                    var exc = '<td>-</td>';
+                    if (item.exclusive_countries.length)
+                        exc = '<td title="' + item.exclusive_countries + '">List</td>';
+                    var dis = '<td>-</td>';
+                    if (item.disallowed_countries.length)
+                        dis = '<td title="' + item.disallowed_countries + '">List</td>';
+                    $('#reg').append('<tr><td>' + (++i) + '</td><td>' + item.machine_name + '</td>' + exc + dis + '</tr>');
+                });
+            },
+            error: function(data){
+                alert('error-key');
+            }
+        });
+    });
+
     $('#btn').click(function () {
         $('#info').empty();
         var i = 0;
-        $('#steam-tab').find('.sr-key').each(function () {
-            var title = $.trim($(this).find('.sr-key-heading').text());
-            var key = $.trim($(this).find('.keyfield-text').text());
+        $('.key-redeemer').each(function () {
+            var title = $.trim($(this).find('h4').text().replace('In your Steam library.', ''));
+            var key = $.trim($(this).find('.keyfield-value').text());
             $('#info').append('<tr><td>' + (++i) + '</td><td>' + title + '</td><td>' + key + '</td></tr>');
         });
     });
     $('#key').click(function () {
-        $('.sr-unredeemed-steam-button').find('span').click();
+        $('.keyfield not(.redeemed)').find('span').click();
     });
     $('#gift').click(function () {
-        $('.sr-unredeemed-gift-button').find('span').click();
+        $('.giftfield').click();
     });
 } else {
     m = /games|mobile/.exec(document.URL);
