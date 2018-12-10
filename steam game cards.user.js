e9c3277c49a0e471b351c0946e23acf6df00196a
rusania <rusania@gmail.com>
2018-12-10 16:01:00 +0800

    20181210

--- steam game cards.user.js
+++ steam game cards.user.js
@@ -3,45 +3,72 @@
 // @namespace    http://tampermonkey.net/
 // @description steam game cards prices
 // @include     http*://steamcommunity.com/profiles/*/gamecards/*
-// @include     http*://steamcommunity.com/profiles/*/badges/
+// @include     http*://steamcommunity.com/profiles/*/badges/*
 // @icon        http://steamcommunity.com/favicon.ico
 // @updateURL 	https://github.com/rusania/gm_scripts/raw/master/steam_game_cards.user.js
 // @downloadURL https://github.com/rusania/gm_scripts/raw/master/steam_game_cards.user.js
-// @version     2018.06.15.1
+// @version     2018.12.10.1
 // @run-at      document-end
 // @require     http://libs.baidu.com/jquery/1.10.1/jquery.min.js
+// @grant       GM_addStyle
 // @grant       unsafeWindow
 // ==/UserScript==
 
+var i = 1;
 var m = /badges/.exec(document.URL);
 if (m){
-    var lv = Math.floor($('.friendPlayerLevelNum').text() / 10) + 1;
-    var xp = $('.profile_xp_block_xp').text().replace(/,/, '');
-    m = /\d+/.exec(xp);
+    m = /p=/.exec(document.URL);
     if (m){
-        xp = 0;
-        for(var i=1;i<=lv;i++)
-            xp += i * 1000;
-        xp -= m[0];
-        $('.profile_small_header_text').append('Lvl:&nbsp;' + lv * 10);
-        $('.profile_small_header_text').append('&nbsp;Exp:&nbsp;' + xp);
-        var cd = Math.ceil(xp/100) - $('.badge_craft_button').length;
-        $('.profile_small_header_text').append('&nbsp;Card:&nbsp;' + cd);
+        GM_addStyle("table{border:solid 1px;border-collapse:collapse;font-size:16px !important;}");
+        GM_addStyle("td{border:solid 1px;border-collapse:collapse;padding-left:5px;padding-right:5px !important;}");
 
         $('.profile_xp_block').before('<table id="b"></table>');
-        $('.badge_craft_button').each(function(k, v){
-            m = /gamecards\/(\d+)/.exec($(v).attr('href'));
-            $('#b').append(`<tr><td>${k}</td><td><a class="car" href="javascript:void(0);" onclick="craftcard('${m[1]}');">${m[1]}</a></td><td id="${m[1]}"></td></tr>`);
+        $('.badge_row').each(function(k, v){
+            var a = $(this).find('.badge_row_overlay');
+            var app = '';
+            var url = '';
+            if (a.length>0){
+                url = $(a[0]).attr('href');
+                m = /cards\/(\d+)/.exec(url);
+                if (m)
+                    app = m[1];
+            }
+            a = $(this).find('.badge_progress_info');
+            var text = '';
+            if (a.length > 0){
+                m = /(\d+) of (\d+) cards/.exec($(a[0]).text());
+                if (m)
+                    text = m[2] - m[1];
+            }
+            $('#b').append(`<tr><td>${i++}</td><td><a target=_blank href="${url}">${app}</a></td><td>${text}</td><td><a href="javascript:void(0);" onclick="getcards(${app}, '${url}');">check</a></td><td id="${app}"></td></tr>`);
         });
+    } else {
+        var lv = Math.floor($('.friendPlayerLevelNum').text() / 10) + 1;
+        var xp = $('.profile_xp_block_xp').text().replace(/,/, '');
+        m = /\d+/.exec(xp);
+        if (m){
+            xp = 0;
+            for(i=1;i<=lv;i++)
+                xp += i * 1000;
+            xp -= m[0];
+            $('.profile_small_header_text').append('Lvl:&nbsp;' + lv * 10);
+            $('.profile_small_header_text').append('&nbsp;Exp:&nbsp;' + xp);
+            var cd = Math.ceil(xp/100) - $('.badge_craft_button').length;
+            $('.profile_small_header_text').append('&nbsp;Card:&nbsp;' + cd);
+
+            $('.profile_xp_block').before('<table id="b"></table>');
+            $('.badge_craft_button').each(function(k, v){
+                m = /gamecards\/(\d+)/.exec($(v).attr('href'));
+                $('#b').append(`<tr><td>${k}</td><td>${m[1]}</td></tr>`);
+            });
+        }
     }
-
-
 } else {
     var match = /sessionid=([a-z0-9]+);/.exec(document.cookie);
     match = /gamecards\/(\d+)/.exec(location.href);
     var id = match[1];
     var collect = $('.badge_cards_to_collect').html();
-    var i = 0;
+    i = 0;
     $('.badge_detail_tasks').before('<table id="bl"></table><br><a href="javascript:void(0);" onclick="buyall();">购买全部</a>');
     $('.badge_card_set_card.unowned').each(function () {
         var un = $(this).find('.badge_card_set_text.ellipsis') [0];
@@ -91,6 +118,51 @@ unsafeWindow.buyall = function(){
     });
 }
 
+unsafeWindow.getcards = function(id, url){
+    var k = `${id}000`;
+    id = `#${id}`;
+    $(id).empty();
+    $.ajax({
+        url: url,
+        type: 'GET',
+        async: true,
+        crossDomain: true,
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function (data) {
+        var r = /market\/listings\/753\/([0-9]+)\-([^\/'"]+)/ig;
+        while (m = r.exec(data)){
+            var hash = `${m[1]}-${m[2]}`;
+            var t = decodeURIComponent(m[2]);
+            $(id).append(`<tr><td><a target=_blank href="/${m[0]}">${t}</a></td><td id=${k}></td></tr>`);
+            getcardprice(k++, hash);
+        }
+    }).fail(function (xhr) {
+    });
+}
+
+unsafeWindow.getcardprice = function(k, hash){
+    var id = `#${k}`;
+    var url = `/market/priceoverview/?country=CN&currency=23&appid=753&market_hash_name=${hash}`;
+    var lowest = '';
+    $.getJSON(url, function (data) {
+        if (data.success === true) {
+            var l = data.lowest_price;
+            if (l){
+                var m = /(\d+)\.(\d+)/.exec(l);
+                var p = parseInt(m[1], 10) * 100 + parseInt(m[2], 10);
+                hash = decodeURIComponent(hash);
+                $(id).append(`<a href="javascript:void(0);" onclick="createbuyorder('${hash}',${p},${k});">${l}</a>`);
+                $(id).after(`<td id="c${k}"></td>`);
+            }
+        }
+    }).done(function () {
+    }).fail(function (xhr) {
+        $(id).append(xhr.statusText);
+    });
+}
+
 unsafeWindow.createbuyorder = function(hash, p, id){
     var da = {
         sessionid: g_sessionID,
@@ -123,8 +195,8 @@ unsafeWindow.createbuyorder = function(hash, p, id){
         {
             $(w).append(data.message);
         }
-    }).fail(function (jqxhr) {
-        alert(jqxhr);
+    }).fail(function (xhr) {
+        alert(xhr);
     });
 }
 
@@ -143,8 +215,8 @@ unsafeWindow.getbuyorderstatus = function(buy_orderid, w){
             $(w).append(buy_orderid);
         else
             $(w).append(data);
-    }).fail(function (jqxhr) {
-        alert(jqxhr);
+    }).fail(function (xhr) {
+        alert(xhr);
     });
 }
 
@@ -164,21 +236,14 @@ unsafeWindow.craftcard = function(id){
     }).done(function (data) {
         if (data.success === 1)
         {
-            if (data.Badge){
-                $(w).append(`<div>${data.Badge.game}&#9;${data.Badge.title}</div>`);
-            }
-            $.each(data.rgDroppedItems, function(i, v){
-                if (v.level)
-                    $(w).append(`<div>${v.level}</div>`);
-                else
-                    $(w).append(`<div>${v.title}</div>`);
-            });
+            var buy_orderid = data.buy_orderid;
+            setTimeout(getbuyorderstatus(buy_orderid, w), 3000);
         }
         else
         {
-            $(w).append(JSON.stringify(data));
+            $(w).append(data.message);
         }
-    }).fail(function (jqxhr) {
-        $('#'+id).append(jqxhr);
+    }).fail(function (xhr) {
+        $('#'+id).append(xhr);
     });
 }
